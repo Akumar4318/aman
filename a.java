46cfc51bee18a0a969d541b5d5142f9d9180a3a6
Abhishek kumar <bhhek@Abhisheks-MacBook-Air.local>
2023-06-18 03:58:11 +0530

    create a java file

---
+++ a.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+
+public class a {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        long n= sc.nextLong();
+        long factorial= 1;
+        for(int i=1;i<=n;i++)
+        {
+            factorial=factorial*i;
+        }
+System.out.println(factorial);
+    }
+}
