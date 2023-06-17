765ed4d6234b2b6473475f53599aeacf022e61a3
Abhishek kumar <bhhek@Abhisheks-MacBook-Air.local>
2023-06-18 04:00:58 +0530

    change from long to int

--- a.java
+++ a.java
@@ -5,8 +5,8 @@ import java.io.*;
 public class a {
     public static void main(String arg[]){
         Scanner sc = new Scanner(System.in);
-        long n= sc.nextLong();
-        long factorial= 1;
+        int  n= sc.nextInt();
+        int factorial= 1;
         for(int i=1;i<=n;i++)
         {
             factorial=factorial*i;
